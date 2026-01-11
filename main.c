1e12558b5d8abe8524d886529b36fbabc90b66e4
Selim Efe <uysalselimefe@gmail.com>
2026-01-11 13:20:16 +0300

    Implement positive filter and mean calculation logic
    Added source code that aggregates valid positive inputs into a buffer until a termination signal (-1) is received, then computes the floating-point average.

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+            int sayi,i,array[999],index=0,sonuc,ort;
+
+    ort=0;
+
+
+    for(;;){
+        printf("sayi girin");
+        scanf("%d",&sayi);
+
+        if(sayi==-1){
+            break;
+        }
+        if(sayi>0){
+            array[index]=sayi;
+            index++;
+        }
+
+    }
+    for(i=0;i<index;i++){
+        ort+=array[i];
+    }
+    sonuc=ort/index;
+    printf("sonuc: %d",sonuc);
+
+
+    return 0;
+}
